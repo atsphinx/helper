8c336180a92b494f5ada4d390d6d4a674578343a
Kazuya Takei <myself@attakei.net>
2024-03-16 20:34:07 +0900

    feat: Initial commit
    - Run 'rye init'
    - Copy personal files
    - Configure for namespace package

---
+++ doc/conf.py
@@ -0,0 +1,16 @@
+from atsphinx.helpers import __version__
+
+# -- Project information -----------------------------------------------------
+project = "atsphinx-helpers"
+copyright = "2024, Kazuya Takei"
+author = "Kazuya Takei"
+release = __version__
+
+# -- General configuration ---------------------------------------------------
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = "furo"
+html_static_path = ["_static"]
