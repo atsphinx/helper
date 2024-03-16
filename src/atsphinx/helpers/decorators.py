5f812f448a94424416844b576f35ad15c2a0f780
Kazuya Takei <myself@attakei.net>
2024-03-17 04:01:54 +0900

    feat: User can custom loglevel

--- src/atsphinx/helpers/decorators.py
+++ src/atsphinx/helpers/decorators.py
@@ -1,6 +1,7 @@
 """Helpful decorators."""
 
 import functools
+import logging
 from typing import Callable, List, Optional, Union
 
 from sphinx.application import Sphinx
@@ -9,7 +10,12 @@ from sphinx.util.logging import getLogger
 Logger = getLogger(__name__)
 
 
-def only(*, builders: Optional[List[str]] = None, formats: Optional[List[str]] = None):
+def only(
+    *,
+    builders: Optional[List[str]] = None,
+    formats: Optional[List[str]] = None,
+    loglevel: int = logging.WARN,
+):
     """Restict extension by builder types or formats.
 
     This is decorator function.
@@ -28,8 +34,9 @@ def only(*, builders: Optional[List[str]] = None, formats: Optional[List[str]] =
     :params formats: List of format types for restrict target.
     """
     if builders is None and formats is None:
-        Logger.warning(
-            "To use @only, you should set argument builders or formats at least."
+        Logger.log(
+            loglevel,
+            "To use @only, you should set argument builders or formats at least.",
         )
 
     def _only(func: Callable[[Sphinx], Union[dict, None]]):
@@ -38,12 +45,13 @@ def only(*, builders: Optional[List[str]] = None, formats: Optional[List[str]] =
         def _restrict_builder(app: Sphinx):
             target = func.__module__ or "setup() on conf.py"
             if builders and app.builder.name not in builders:
-                logger.warning(
-                    f"{target} is not supported '{app.builder.name}' builder."
+                logger.log(
+                    loglevel, f"{target} is not supported '{app.builder.name}' builder."
                 )
             if formats and app.builder.format not in formats:
-                logger.warning(
-                    f"{target} is not supported '{app.builder.format}' format."
+                logger.log(
+                    loglevel,
+                    f"{target} is not supported '{app.builder.format}' format.",
                 )
 
         @functools.wraps(func)
